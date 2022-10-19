b5dccf414ba2e9cf23e64c29613736dfad8ca37a
Bobbie Soedirgo <bobbie@soedirgo.dev>
2022-10-19 16:36:59 +0800

    fix: authenticate action requests to github api (#78)
    * fix: only test on latest
    
    Should help with GitHub API rate limiting
    
    * Update .github/workflows/test.yml
    
    * fix: add github token to workflow
    
    * fix: authenticate with github api
    
    * chore: remove token env
    
    * chore: update dist files
    
    * Revert "chore: remove token env"
    
    This reverts commit 913c7a8e6ff1f18a2987fdc41d66b8275e961035.
    
    * chore: use gh token env var
    
    * chore: update user agent string
    
    * chore: organize imports
    
    Co-authored-by: Han Qiao <qiao@supabase.io>

--- dist/index.js
+++ dist/index.js
@@ -71,29 +71,6 @@ run();
 
 "use strict";
 
-var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    var desc = Object.getOwnPropertyDescriptor(m, k);
-    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
-      desc = { enumerable: true, get: function() { return m[k]; } };
-    }
-    Object.defineProperty(o, k2, desc);
-}) : (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    o[k2] = m[k];
-}));
-var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
-    Object.defineProperty(o, "default", { enumerable: true, value: v });
-}) : function(o, v) {
-    o["default"] = v;
-});
-var __importStar = (this && this.__importStar) || function (mod) {
-    if (mod && mod.__esModule) return mod;
-    var result = {};
-    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
-    __setModuleDefault(result, mod);
-    return result;
-};
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
@@ -109,7 +86,8 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", ({ value: true }));
 exports.getDownloadUrl = void 0;
 const os_1 = __importDefault(__nccwpck_require__(2037));
-const httpm = __importStar(__nccwpck_require__(6255));
+const http_client_1 = __nccwpck_require__(6255);
+const auth_1 = __nccwpck_require__(5526);
 // arch in [arm, arm64, x64...] (https://nodejs.org/docs/latest-v16.x/api/os.html#osarch)
 // return value in [amd64, arm64, arm]
 const mapArch = (arch) => {
@@ -134,12 +112,14 @@ const getDownloadUrl = (version) => __awaiter(void 0, void 0, void 0, function* 
     return `https://github.com/supabase/cli/releases/download/v${resolvedVersion}/${filename}.tar.gz`;
 });
 exports.getDownloadUrl = getDownloadUrl;
+// Authenticate with GH_TOKEN to avoid GitHub API rate limits
+const token = process.env['GH_TOKEN'];
+const http = new http_client_1.HttpClient('supabase/setup-cli', token ? [new auth_1.BearerCredentialHandler(token)] : undefined);
 const resolveVersion = (version) => __awaiter(void 0, void 0, void 0, function* () {
     var _a;
     if (version !== 'latest') {
         return version;
     }
-    const http = new httpm.HttpClient('setup-cli');
     const url = 'https://api.github.com/repos/supabase/cli/releases/latest';
     const tag = (_a = (yield http.getJson(url)).result) === null || _a === void 0 ? void 0 : _a.tag_name;
     if (!tag) {
