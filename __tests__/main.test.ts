9836fc35fcc3e3333dc3c9fe8dee19b3487cfa18
Qiao Han <qiao@supabase.io>
2023-01-30 17:23:23 +0800

    chore: parse default action input

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -4,6 +4,8 @@ import * as os from 'os'
 import * as process from 'process'
 import * as cp from 'child_process'
 import * as path from 'path'
+import * as fs from 'fs'
+import * as yaml from 'js-yaml'
 import {expect, test} from '@jest/globals'
 
 test('gets download url to binary', async () => {
@@ -37,6 +39,9 @@ test('gets download url to latest version', async () => {
 // shows how the runner will run a javascript action with env / stdout protocol
 test('test runs', () => {
   process.env['RUNNER_TEMP'] = os.tmpdir()
+  const config = path.join(__dirname, '..', 'action.yml')
+  const action: any = yaml.load(fs.readFileSync(config, 'utf8'))
+  process.env['INPUT_VERSION'] = action.inputs.version.default
   const np = process.execPath
   const ip = path.join(__dirname, '..', 'lib', 'main.js')
   const options: cp.ExecFileSyncOptions = {
